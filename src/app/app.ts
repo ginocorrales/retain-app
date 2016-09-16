b3421abcea2775e978f0b64958191cfb23f6967a
Gino Corrales <ginocorrales@Ginos-MacBook-Pro.local>
2016-09-15 23:35:43 -0500

    apps.ts
    adding app.ts

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app',
+  template: '
+    <div>
+      <h3>
+        Yo Gino.. and World!
+      </h3>
+    </div>
+  '
+})
+export class App {}
